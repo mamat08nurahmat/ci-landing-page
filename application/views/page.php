36e01094ce8f9bd6cfce5d22848bd7900e328c28
mamat08nurahmat <mamat08nurahmat@gmail.com>
2019-05-20 21:46:39 +0700

    UPDATE TITLE

--- application/views/page.php
+++ application/views/page.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>TITLE TES</title>
+    <title>UPDATE TITLE</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
